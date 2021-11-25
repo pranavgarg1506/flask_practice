09d68c60a3b169ac756e829ec94ab5a732794e5a
Pranav Garg <pranavfunny1@gmail.com>
2021-11-25 13:13:36 +0530

    v0.2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from logging import debug
-from flask import Flask
+from flask import Flask, url_for, redirect
 import flask
-from flask import Flask
+
 
 print("Flask version is",flask.__version__)
 
@@ -18,10 +18,36 @@ def hello_world():
 '''
 https://flask.palletsprojects.com/en/2.0.x/quickstart/#unique-urls-redirection-behavior
 '''
+
+
 @app.route('/home/')
 def hello_pranav():
     print("Routing to /about/ and Entering into hello_pranav function")
     return 'HELLLOOO  WELCOME TO THE SECRET PAGE'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    print("Entering into hello_guest function")
+    return f"Welcome {guest}!"
+
+
+@app.route('/admin')
+def hello_admin():
+    print("Entering into hello_admin function")
+    return "Welcome admin!!!!!!!"
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    print("Entering into hello_user")
+    if name == 'admin':
+        print("ADMIN")
+        return redirect(url_for('hello_admin'))
+    else:
+        print("NAME")
+        return redirect(url_for('hello_guest', guest = name))
+
 
 if __name__ == '__main__':
     print("Runing the application.....................................")
